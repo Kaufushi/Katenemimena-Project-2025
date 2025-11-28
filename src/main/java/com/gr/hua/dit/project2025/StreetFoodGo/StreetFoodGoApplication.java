211f52e41fce5b6e169e80a6acb16aee90e64f5f
Kaufushi <adrinhoda303@gmail.com>
2025-11-28 14:58:30 +0200

    test

---
+++ src/main/java/com/gr/hua/dit/project2025/StreetFoodGo/StreetFoodGoApplication.java
@@ -0,0 +1,13 @@
+package com.gr.hua.dit.project2025.StreetFoodGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetFoodGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetFoodGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gr/hua/dit/project2025/StreetFoodGo/StreetFoodGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gr.hua.dit.project2025.StreetFoodGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetFoodGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
