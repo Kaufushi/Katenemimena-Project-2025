854c5a18abb152aa652cd4b718771b72e4c1eef5
VassilisCodes <stathvassilis@gmail.com>
2025-12-02 19:50:24 +0200

    Model

---
+++ src/main/java/com/gr/hua/dit/project2025/StreetFoodGo/core/model/Person.java
@@ -0,0 +1,102 @@
+package com.gr.hua.dit.project2025.StreetFoodGo.core.model;
+
+import java.time.Instant;
+
+public class Person {
+
+    private String firstname;
+    private String lastname;
+    private Long id;
+    private PersonType type;
+    private String emailAddress;
+    private String mobileNumber;
+    private String physicalAddress;
+    private Instant createdAt;
+    private String passwordHash;
+
+    public Person() {}
+
+    public Person(Instant createdAt, PersonType type, String physicalAddress, String passwordHash, String mobileNumber, String lastname, Long id, String firstname, String emailAddress) {
+        this.createdAt = createdAt;
+        this.type = type;
+        this.physicalAddress = physicalAddress;
+        this.passwordHash = passwordHash;
+        this.mobileNumber = mobileNumber;
+        this.lastname = lastname;
+        this.id = id;
+        this.firstname = firstname;
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhysicalAddress() {
+        return physicalAddress;
+    }
+
+    public void setPhysicalAddress(String physicalAddress) {
+        this.physicalAddress = physicalAddress;
+    }
+
+    public PersonType getType() {
+        return type;
+    }
+
+    public void setType(PersonType type) {
+        this.type = type;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
---
+++ src/main/java/com/gr/hua/dit/project2025/StreetFoodGo/core/model/PersonType.java
@@ -0,0 +1,5 @@
+package com.gr.hua.dit.project2025.StreetFoodGo.core.model;
+
+public enum PersonType {
+    CUSTOMER , OWNER
+}
